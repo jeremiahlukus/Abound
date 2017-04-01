e1ccc911f985a9ba82ce01110ad200f206a46309
Jeremiah Parrack <jeremiahlukus1@gmail.com>
2017-04-01 01:10:34 -0400

    Hosting default on AWS

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,5 @@
 class Listing < ApplicationRecord
-  has_attached_file :image, styles: { medium: "200x>", thumb: "100x100>" }, default_url: "default.jpeg"
+  has_attached_file :image, styles: { medium: "200x>", thumb: "100x100>" }, default_url: "https://s3.amazonaws.com/abound/default.jpg"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
 end
